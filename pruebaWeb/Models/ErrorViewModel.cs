a8566ab6dd329bb58553c7f30ab2bc2e406ef9a4
Alejandra-pinzon-camelo <lalejandrapinzon@ucundinamarca.edu.co>
2023-08-29 19:38:30 -0500

    nuevo

---
+++ pruebaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pruebaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
